4063e9192977c5faab7f496ecf50673d211a45f4
ivarafa <ivar.fauske@hotmail.com>
2018-03-21 10:20:21 +0100

    outline of main and ComInput

--- ComInput.py
+++ ComInput.py
@@ -1,1 +1,20 @@
 # This Python file uses the following encoding: utf-8
+
+class Connection:
+
+    #TODO: Code to establish connection
+    def __init__(self, user):
+        ""
+
+    #TODO: Need a function to listen to incoming messages
+    def recieve(self):
+        ""
+
+    #TODO: Function to send messages over the connection
+    def send(self, message):
+        ""
+
+    #TODO: If relevant knowledge, inform user of connection
+    def informUser(self, message):
+        ""
+
--- main.py
+++ main.py
@@ -1,1 +1,21 @@
 # This Python file uses the following encoding: utf-8
+
+import DecisionMaker
+import MotorInterface
+import SensorReader
+import ComInput
+
+
+class RobotProgram:
+
+    def __init__(self):
+        ""
+
+    #TODO: Should make use of threading!
+    def run(self):
+        ""
+
+
+if __name__ == '__main__':
+    program = RobotProgram()
+    program.run()
