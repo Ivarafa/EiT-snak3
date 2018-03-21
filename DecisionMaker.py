c1fb051a578082189167eb25f03da5c5bdae6246
ivarafa <ivar.fauske@hotmail.com>
2018-03-21 11:24:50 +0100

    outline for superclass DecisionMaker

--- DecisionMaker.py
+++ DecisionMaker.py
@@ -1,1 +1,14 @@
 # This Python file uses the following encoding: utf-8
+
+#this class is meant as an abstract superclass with it's methods overwritten
+class DecisionMaker:
+    #init should set relevant static info
+    def __init__(self):
+        ""
+
+    def updateKnowledge(self):
+        raise NotImplementedError
+
+    def getDecision(self):
+        raise NotImplementedError
+
